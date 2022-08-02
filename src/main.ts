0b54368085b08c47b6932fca663b60f19793abc7
SuperCuteXiaoSi <1531733886@qq.com>
2022-08-03 00:11:26 +0800

    feature: 添加校验

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
+import { createSSRApp } from 'vue';
+import App from './App.vue';
 export function createApp() {
   const app = createSSRApp(App);
   return {
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,34 +8,31 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+  import { ref } from 'vue';
+  const title = ref('Hello');
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+  .logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin: 200rpx auto 50rpx;
+  }
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
+  .text-area {
+    display: flex;
+    justify-content: center;
+  }
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+  .title {
+    font-size: 36rpx;
+    color: #8f8f94;
+  }
 </style>
